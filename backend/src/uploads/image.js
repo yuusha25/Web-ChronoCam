305eb248057325507ba13d8160339db69f6bba3d
Razky <145090597+yuusha25@users.noreply.github.com>
2025-01-06 02:04:27 +0700

    Update image.js

--- backend/src/uploads/image.js
+++ backend/src/uploads/image.js
@@ -1,34 +1,26 @@
-// import express from "express";
-// import { uploadMedia } from "./imagekit.controler.js";
-// import { authMiddleware } from "../middleware/auth.js";
-// import { uploadMiddleware } from "../middleware/upload.js";
-// import rateLimit from "express-rate-limit";
-
-// const router = express.Router();
-
-// const uploadLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 10, // Limit each IP to 10 uploads per windowMs
-//   message: "Too many uploads from this IP, please try again later",
-// });
-
-// router.post(
-//   "/",
-//   // authMiddleware,
-//   uploadLimiter,
-//   uploadMiddleware.array("foto", 5), // Limit to 5 files max
-//   uploadMedia
-// );
-
 import express from "express";
 import { uploadMedia } from "./imagekit.controler.js";
-import multer from "multer";
+import { authMiddleware } from "../middleware/auth.js";
+import { uploadMiddleware } from "../middleware/upload.js";
+import rateLimit from "express-rate-limit";
 
 const router = express.Router();
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 1 * 1024 * 1024 * 1024 },
+
+const uploadLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 uploads per windowMs
+  message: "Too many uploads from this IP, please try again later",
 });
+
+router.post(
+  "/",
+  // authMiddleware,
+  uploadLimiter,
+  uploadMiddleware.array("foto", 5), // Limit to 5 files max
+  uploadMedia
+);
+
+
 
 router.post("/", upload.array("foto"), uploadMedia);
 
